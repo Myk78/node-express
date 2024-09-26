c540a9882edb7e423dff897cdbba5a4473894f89
Myk78 <nwbyaseen@gmail.com>
2024-09-27 03:03:13 +0500

    add cart function

--- bageyShop/routes/index.js
+++ bageyShop/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { isloggedIn } = require("../middleware/isloggedIn");
 const productModel = require("../models/productModel");
+const userModel = require("../models/userModel");
 
 router.use((req, res, next) => {
   res.locals.loggedIn = req.cookies.token ? true : false;
@@ -16,7 +17,34 @@ router.get("/", (req, res) => {
 
 router.get("/dashboard", isloggedIn, async (req, res) => {
   let products = await productModel.find();
-  res.render("shop", { products });
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isloggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+  res.render("cart", { user});
+});
+
+router.get("/cart/:productid", isloggedIn, async (req, res) => {
+  try {
+    let user = await userModel.findOne({ email: req.user.email });
+    if (!user) return res.status(404).send("User not found");
+
+    if (!user.cart) {
+      user.cart = []; // Initialize cart if it doesn't exist
+    }
+    user.cart.push(req.params.productid);
+    await user.save();
+
+    req.flash("success", "You added a product to the cart");
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occurred");
+  }
 });
 
 module.exports = router;
